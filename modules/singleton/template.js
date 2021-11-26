419b8f57588e5c4d937281cee17f9b6abfa38e75
torikushiii <joerdyl@gmail.com>
2021-11-27 02:18:23 +0800

    added singleton template

---
+++ modules/singleton/template.js
@@ -0,0 +1,26 @@
+module.exports = class SingletonTemplate {
+    /**
+     * Cleans up the module.
+     * All submodules must implement this method.
+     * @abstract
+     */
+    destroy () {
+        throw new Error('SingletonTemplate::destroy() is not implemented');
+    }
+
+    /**
+     * Construct the singleton instance.
+     * @return {Promise<void>}
+     */
+    static singleton () {
+        throw new Error('SingletonTemplate::singleton() is not implemented');
+    }
+
+    /**
+     * File name of the module.
+     * All submodules must implement this method.
+     */
+    get modulePath () {
+        throw new Error('SingletonTemplate::modulePath() is not implemented');
+    }
+}
