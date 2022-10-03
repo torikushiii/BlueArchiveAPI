cf500b6ae990f66e29ea65e7c6b90ef9f162912a
torikushiii <torikushiii@gmail.com>
2022-10-03 20:57:31 +0800

    added banner route

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const logger = require("./lib/logger");
 		"character",
 		"equipment",
 		"stage",
-		"raid"
+		"raid",
+		"banner"
 	];
 
 	const config = ba.Config;
---
+++ routes/banner.js
@@ -0,0 +1,17 @@
+module.exports = function (fastify, opts, done) {
+	const Router = fastify;
+
+	Router.get("/", async (req, res) => {
+		const data = await ba.Utils.getBannerData();
+		if (data.current.length === 0 && data.past.length === 0) {
+			return res.notFound("No banners found");
+		}
+        
+		res.send({
+			status: 200,
+			data
+		});
+	});
+
+	done();
+};
